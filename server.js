e1965768bbabf19ae624df697c0fcdd505000048
Robby Hill <88348635+robbyhill20@users.noreply.github.com>
2021-12-08 09:01:55 -0800

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.static('public'));
 
 
 app.get('/', (req, res) =>
-res.sendFile(path.join(__dirname, '/index.html'))
+res.sendFile(path.join(__dirname, '/Develop/public/index.html'))
 );
 
 app.get('/notes', (req, res) =>
